1c0929e35c04f9e48fd1191bdd2e37e33ab36736
Felix Ramon <mail@felixramon.net>
2016-09-23 14:47:08 +0200

    graphic, light and cleaning folders

---
+++ Assets/clic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("Mouse1"))
+            GetComponent<Animation>().Play("mallowSeduction");
+
+    }
+}
